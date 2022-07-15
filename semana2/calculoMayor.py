342facb2c3e84fe0856f9714a3c71771f4d98f85
Andres Gutierrez <amgutierrezc19@hotmail.com>
2022-07-14 23:16:42 -0500

    estructura algoritmo con comentarios

--- semana2/calculoMayor.py
+++ semana2/calculoMayor.py
@@ -1,8 +1,11 @@
 #pedirle al usuario dos valores numericos. imprimir el mayor de ellos.
 
+
+#se solicitan al usuario los dos numeros
 valor1 = int(input("Ingrese el primer numero: "))
 valor2 = int(input("Ingrese el segundo numero: "))
 
+#se crea la logica del programa y se imprime en pantalla.
 if valor1 > valor2:
     print(f"el número con mayor valor es : {valor1}, que es el primer numero ingresado")
 if valor1 == valor2:
